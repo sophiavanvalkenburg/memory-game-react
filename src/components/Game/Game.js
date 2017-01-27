1da0a021afa3bf0d76bd2f53f08ff672dd57c295
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2017-01-26 20:00:58 -0500

    add cards to game board (first pass)

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,12 +3,94 @@ import React from 'react'
 import Timer from '../Timer/Timer'
 import styles from './Game.scss'
 
-const Game = () => (
-  <div>
-    <h1 className={styles.header}>NYT Games Code Test</h1>
-    <Timer />
-    <div className={styles.placeholder}>Let the games begin (here).</div>
-  </div>
+function getGameData(){
+  return {
+    "levels": [
+    {
+      "cards": [
+        "✈",
+        "♘",
+        "✈",
+        "♫",
+        "♫",
+        "☆",
+        "♘",
+        "☆"
+      ],
+      "difficulty": "easy"
+    },
+    {
+      "cards": [
+        "❄",
+        "⍨",
+        "♘",
+        "✈",
+        "☯",
+        "♠",
+        "☆",
+        "❄",
+        "♫",
+        "♫",
+        "☯",
+        "☆",
+        "✈",
+        "⍨",
+        "♠",
+        "♘"
+      ],
+      "difficulty": "hard"
+    }
+    ]
+  } 
+}
+
+const Card = props => (
+  <button className={styles.boardCard} onClick={() => alert("click")}>
+    {props.content}
+  </button>
 )
+
+class Board extends React.Component {
+  
+  renderRow(nRow, rowData){
+    let row = [];
+    for (let i=0; i<4; i++){
+      row.push(<Card key={`card-${nRow}-${i}`} content={rowData[i]}/>);
+    }
+    return ( <div key={`row-${nRow}`} className={styles.boardRow}>{ row }</div>);
+  }
+
+  renderBoard(){
+    let board = [];
+    for (let i=0; i<2; i++){
+      const rowData = this.props.cardData.slice(i * 4, i * 4 + 4);
+      board.push(this.renderRow(i, rowData));
+    }
+    return (<div>{ board }</div>);
+  }
+
+  render(){
+    return this.renderBoard();
+  }
+}
+
+class Game extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      gameData: getGameData()
+    }
+  }
+  render() {
+    const cardData = this.state.gameData.levels[0].cards.slice();
+    return (
+      <div>
+        <h1 className={styles.header}>NYT Games Code Test</h1>
+        <Timer />
+        <div><Board cardData={cardData} /></div>
+      </div>
+    )
+  }
+}
 
 export default Game
